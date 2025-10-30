7e8050382b7e4f8cbae47d60feb772ae1add3f23
nazar <nazarvusik77@gmail.com>
2025-10-30 17:20:44 +0300

    сделал возможность писать большие никнеймы

--- website/mainSite/static/main/js/reg.js
+++ website/mainSite/static/main/js/reg.js
@@ -54,7 +54,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Проверка имени пользователя
     function checkUsernameMatch() {
         const username = elements.usernameInput.value;
-        return username.length >= 5 && username.length <= 15;
+        return username.length >= 5 && username.length <= 50;
     }
 
     // Расчет рейтинга пароля
